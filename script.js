62b8fca75cd9d792a4192e8cbe91a0b6ccd38c14
halsschmerzen <bastiansteampl@gmail.com>
2023-09-05 11:55:49 +0200

    Wrote JS Logic, added according CSS

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-const squareContainer = document.querySelector(".sketch-container");
+const squareContainer = document.getElementById("sketch-container");
 
+// squareContainer.style.border = "2px solid red";
+
+function makeRows(rows, columns) {
+    // Create Rows
+  for (let i = 0; i < rows; i++) {
+    let row = document.createElement("div");
+    squareContainer.appendChild(row);
+    row.className = "divRow";
+
+    // Create Columns
+
+    for(let i = 0; i < columns; i++) {
+        let column = document.createElement("div");
+        row.appendChild(column);
+        column.className = "divColumn";
+
+    }
+
+  }
+}
+
+makeRows(8,8);
